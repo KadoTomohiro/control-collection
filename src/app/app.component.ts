cbd99aa19e5b22815f8cbb0b4c0ee7e0bcc9c3a0
KadoTomohiro <corner0427@gmail.com>
2024-06-23 01:15:33 +0900

    ヘッダーにトップリンク追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {ControlListPageComponent} from "@pages/control-list-page/control-list-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ControlListPageComponent],
+  imports: [RouterOutlet, ControlListPageComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
