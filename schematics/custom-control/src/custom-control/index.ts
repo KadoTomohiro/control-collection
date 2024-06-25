620d0576cbeabcd7440dbadc6bb4e5be70aeef93
KadoTomohiro <>
2024-06-25 12:04:41 +0900

    [WIP]CustomControlSchimatics実装

--- schematics/custom-control/src/custom-control/index.ts
+++ schematics/custom-control/src/custom-control/index.ts
@@ -1,5 +1,13 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
+import { Schema as CustomControlShema } from './schema';
+
+// generate function of custom-control
+export function generate(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return tree;
+  };
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
