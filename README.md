399140615b319205e4a17408d0940970c0d3997d
KadoTomohiro <corner0427@gmail.com>
2024-06-26 02:40:10 +0900

    Custom Control Shematicsd

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
 # ControlCollection
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.3.
 
-## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+## Setup
 
-## Code scaffolding
+```shell
+npm install
+```
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## Custom Control Schematics
 
-## Build
+Setup schematics.
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+```shell
+npm run build:schema
+```
 
-## Running unit tests
+Generate Control.
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+```shell
+ng g ccs:control controls/<control-name>
+```
 
-## Running end-to-end tests
+## Publish
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+https://kadotomohiro.github.io/control-collection/
---
+++ schematics/src/control/index.ts
@@ -0,0 +1,69 @@
+import {
+  apply,
+  applyTemplates, chain, mergeWith, move,
+  Rule,
+  SchematicsException, strings,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import {normalize, virtualFs, workspaces} from "@angular-devkit/core";
+
+import {Schema as ControlSchema} from './schema';
+import {parseName} from "./parseName";
+
+
+function createHost(tree: Tree): workspaces.WorkspaceHost {
+  return {
+    async readFile(path: string): Promise<string> {
+      const data = tree.read(path);
+      if (!data) {
+        throw new SchematicsException('File not found.');
+      }
+      return virtualFs.fileBufferToString(data);
+    },
+    async writeFile(path: string, data: string): Promise<void> {
+      return tree.overwrite(path, data);
+    },
+    async isDirectory(path: string): Promise<boolean> {
+      return !tree.exists(path) && tree.getDir(path).subfiles.length > 0;
+    },
+    async isFile(path: string): Promise<boolean> {
+      return tree.exists(path);
+    },
+  };
+}
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function control(options: ControlSchema): Rule {
+  return async (tree: Tree) => {
+    const host = createHost(tree);
+    const {workspace} = await workspaces.readWorkspace('/', host);
+    const project = options.project != null ? workspace.projects.get(options.project) : null;
+    if (!project) {
+      throw new SchematicsException(`Invalid project name: ${options.project}`);
+    }
+    const projectType = project.extensions.projectType === 'application' ? 'app' : 'lib';
+    if (options.path === undefined) {
+      options.path = `${project.sourceRoot}/${projectType}`;
+    }
+
+    const parsedPath = parseName(options.path, options.name);
+    options.name = parsedPath.name;
+    options.path = parsedPath.path;
+
+    const prefix = project.prefix
+
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: options.name,
+        prefix,
+      }),
+      move(normalize(options.path as string)),
+    ]);
+    return chain([mergeWith(templateSource)]);
+  };
+}
