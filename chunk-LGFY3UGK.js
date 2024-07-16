1fbdb2d42ffefc5d3609de4d9601710fb9869b6b
KadoTomohiro <mail@example.com>
2024-07-16 16:15:31 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/KadoTomohiro/control-collection/commit/00392a7356870ba78f775d04d4cb7a6f23f4c8b4

--- chunk-LGFY3UGK.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-LGQH6H42.js";var t=[{path:"",loadComponent:()=>import("./chunk-W7AYF47S.js").then(o=>o.ControlDocumentComponent)},{path:"demo",loadComponent:()=>import("./chunk-BZHDXJBP.js").then(o=>o.DemoTabComponent)}];export{t as controlPageRoute};
